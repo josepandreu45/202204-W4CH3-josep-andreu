f6679c5aa2ef417b67f176bd7f86d6229af999c0
josep andreu <josepa45@gmail.com>
2022-04-27 20:15:54 +0200

    creacion componente button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import KeyComponent from "./components/KeyComponent/KeyComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="keyboard-container">
+        <ol className="keyboard">
+          <KeyComponent className={"key"} text={"1"} />
+        </ol>
+      </div>
     </div>
   );
 }
